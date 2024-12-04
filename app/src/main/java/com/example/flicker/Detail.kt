aa1b7dba28edc6020c4b07ad81e6fbe7a25683b7
Pau Pedros <paupedros10@gmail.com>
2024-12-04 19:27:04 +0100

    Activity detall movie

--- app/src/main/java/com/example/flicker/Detail.kt
+++ app/src/main/java/com/example/flicker/Detail.kt
@@ -1,19 +1,16 @@
 package com.example.flicker
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
 
-class Movies : AppCompatActivity() {
+class Detail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_movies)
+        setContentView(R.layout.activity_detail)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
