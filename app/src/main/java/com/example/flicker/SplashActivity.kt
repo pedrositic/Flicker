69b4d08c4701c32b5614cf90e6b91e3473535bca
Quim <quipegol@gmail.com>
2025-01-23 18:27:09 +0100

    Animacion v2

--- app/src/main/java/com/example/flicker/SplashActivity.kt
+++ app/src/main/java/com/example/flicker/SplashActivity.kt
@@ -7,6 +7,7 @@ import android.os.Looper
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.LinearLayout
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,15 +15,22 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         val logo: ImageView = findViewById(R.id.splash_logo)
+        val fondo: LinearLayout = findViewById(R.id.splash_layout)
 
-        // Cargar la animación
-        val animacion = AnimationUtils.loadAnimation(this, R.anim.splash_anim)
-        logo.startAnimation(animacion)
+        // Animación de zoom y rebote
+        val animacionLogo = AnimationUtils.loadAnimation(this, R.anim.splash_anim)
+        logo.startAnimation(animacionLogo)
 
-        // Cambiar a MainActivity después de la animación
+        // Retraso para que la animación de entrada termine antes de desvanecer el fondo
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, Login::class.java))
-            finish()
-        }, 2500) // Duración de la animación
+            val animacionFondo = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+            fondo.startAnimation(animacionFondo)
+
+            // Cambiar a MainActivity después del desvanecimiento
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(Intent(this, Login::class.java))
+                finish()
+            }, 800) // Tiempo de la animación fade_out
+        }, 2000) // Tiempo para que termine la animación inicial
     }
 }
