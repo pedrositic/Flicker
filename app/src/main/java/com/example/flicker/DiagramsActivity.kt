7418c6607330b5dd6ee15013c36782266fd43b96
qpelacals <quipegol@gmail.com>
2025-03-25 20:17:11 +0100

    Activity, Layout i llogica botó per enviar a Diagrams

---
+++ app/src/main/java/com/example/flicker/DiagramsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.flicker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DiagramsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diagrams)
+    }
+}
